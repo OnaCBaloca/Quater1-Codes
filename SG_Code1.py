7078ffedd1c5587ac621e95d301147858024fdf7
OnaCBaloca <onakolme.baloca@smc.pshs.edu.ph>
2025-10-01 21:35:23 +0800

    SG_Code1.py
    In this problem I am required to make a program that will ask the user to input 3 numbers and calculate the sum. The sum must be rounded to two decimal places.

---
+++ SG_Code1.py
@@ -0,0 +1,12 @@
+#Input the variables
+x= float(input("Enter 1st number:"))
+y= float(input("Enter 2nd number:"))
+z= float(input("Enter 3rd number:"))
+
+#Compute the 3 numbers
+total_sum= x+y+z
+
+finalsum= round(total_sum,2)
+
+#Display result
+print("The sum of the 3 numbers is:", finalsum)
